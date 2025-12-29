fd11dd86889a3c884d7324eaef741f30e19882e6
Matthmusic <matthieu@maurelfamily.fr>
2025-12-29 11:21:10 +0100

    chore: bump version to 0.0.2

--- electron-react/src/App.tsx
+++ electron-react/src/App.tsx
@@ -11,7 +11,7 @@ type RenderResponse = {
 const hasApi = () => typeof window !== 'undefined' && typeof (window as any).api !== 'undefined'
 
 function App() {
-  const currentVersion = '0.0.1'
+  const currentVersion = '0.0.2'
   const [folder, setFolder] = useState('')
   const [html, setHtml] = useState('')
   const [text, setText] = useState('')
